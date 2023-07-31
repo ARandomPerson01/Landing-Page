10968972b1887a293c808990f815dd1320c60938
Diego Depaz <depazramirezdiego02@gmail.com>
2023-07-31 12:08:07 -0500

    Agregando sliders a carpeta imgs

--- navbar.js
+++ navbar.js
@@ -24,3 +24,25 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//SCROLL SECTION
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop -58,
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.navM a[href*='+ sectionId + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.navM a[href*='+ sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
